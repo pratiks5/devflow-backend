50e2aa57b434b6e8d04f9a9a37c89241c73a07d5
Pratik Sonawane <134759546+prats515@users.noreply.github.com>
2026-06-26 23:59:32 +0530

    added CustomUserDetailsService class.

--- src/main/java/com/devflow/security/CustomUserDetailsService.java
+++ src/main/java/com/devflow/security/CustomUserDetailsService.java
@@ -1,4 +1,22 @@
 package com.devflow.security;
 
-public class CustomUserDetailsService {
+import com.devflow.user.entity.User;
+import com.devflow.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email){
+        User user =
+                userRepository.findByEmail(email)
+                        .orElseThrow(...);
+    }
 }
