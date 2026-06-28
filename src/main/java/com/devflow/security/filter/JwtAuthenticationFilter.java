671e756d33d37f5a88785d03394dd7417bd9f51f
Pratik Sonawane <134759546+prats515@users.noreply.github.com>
2026-06-28 23:43:33 +0530

    added JwtAuthenticationFilter class.

---
+++ src/main/java/com/devflow/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package com.devflow.security.filter;
+
+import com.devflow.security.CustomUserDetailsService;
+import com.devflow.security.jwt.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader =
+                request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwt = authHeader.substring(7);
+
+        String username = jwtService.extractUsername(jwt);
+
+    }
+}
